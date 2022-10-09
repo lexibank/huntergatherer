6d8382504bd54cf437d88e2fbf6cc6aebc360e61
lingulist <mattis.list@lingulist.de>
2022-10-09 11:23:51 +0200

    update new files

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def test_valid(cldf_dataset, cldf_logger):
 
 def test_forms(cldf_dataset):
     assert len(list(cldf_dataset["FormTable"])) == 64607
-    assert any(f["Form"] == "panyi'a-t" for f in cldf_dataset["FormTable"])
+    assert any(f["Form"] == "hanaka" for f in cldf_dataset["FormTable"])
 
 
 def test_parameters(cldf_dataset):
